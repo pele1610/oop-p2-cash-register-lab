1af73febb84c7dfa1482c5e5c861cfcda92a037e
pele1610 <pelecolo56@gmail.com>
2026-06-09 00:18:24 +0300

    Add CashRegister class with add_item, apply_discount and void_last_transaction methods

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,47 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self._discount = discount
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, value):
+        if not isinstance(value, int):
+            print("Not valid discount")
+        elif value < 0 or value > 100:
+            print("Not valid discount")
+        else:
+            self._discount = value
+
+    def add_item(self, item, price, quantity=1):
+        self.total += price * quantity
+        self.items.append(item)
+        self.previous_transactions.append({
+            "item": item,
+            "price": price,
+            "quantity": quantity
+        })
+
+
+    def apply_discount(self):
+        if len(self.previous_transactions) == 0:
+            print("There is no discount to apply.")
+        else:
+            discount_amount = (self.total * self.discount) / 100
+            self.total = self.total - discount_amount
+
+    def void_last_transaction(self):
+        if len(self.previous_transactions) == 0:
+            print("There is no transaction to void.")
+        else:
+            last = self.previous_transactions[-1]
+            self.total -= last["price"] * last["quantity"]
+            self.items.remove(last["item"])
+            self.previous_transactions.pop()
